8ddab09bf0a061c008f941c937684aca68209987
Guy Bitan <guy.bitan@gmail.com>
2022-01-08 19:13:30 +0200

    Add

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,5 +1,17 @@
-describe("My first simple test", () => {
-  it("True Should be True", () => {
-      expect(true).to.equal(true)
+describe("Browser Actions", () => {
+  it("Should load current url", () => {
+    cy.visit("https://example.com/", { timeout: 10000 });
+  });
+  it("Should check current url", () => {
+    cy.url().should("include", "example.com");
+  });
+  it("Should wait for 3 seconds", () => {
+    cy.wait(30000);
+  });
+  it("check correct element in the page", () => {
+    cy.get("h1").should("be.visible");
+  });
+  it("check incorrect element in the page", () => {
+    cy.get("h6").should("not.be.visible");
   });
 });
