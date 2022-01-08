94382510c0cfa284818fe229fa6b8997966c11dc
Guy Bitan <guy.bitan@gmail.com>
2022-01-08 20:17:41 +0200

    Add

--- cypress/integration/examples/count-of-elementse.spec.js
+++ cypress/integration/examples/count-of-elementse.spec.js
@@ -1,15 +1,15 @@
-describe("Browser Actions", () => {
-  it("Should load current url", () => {
-    cy.visit("http://books.toscrape.com/index.html", { timeout: 10000 });
-  });
-  it("Should check current url", () => {
-    cy.url().should("include", "index.html");
-  });
-  it("Should click on Travel link category", () => {
-    cy.get("a").contains("Travel").click();
-    cy.get("h1").contains("Travel");
-  });
-  it("Should display correct number of books", () => {
-    cy.get("");
-  });
-});
+describe('Browser Actions', () => {
+	it('Should load current url', () => {
+		cy.visit('http://books.toscrape.com/index.html', { timeout: 10000 })
+	})
+	it('Should check current url', () => {
+		cy.url().should('include', 'index.html')
+	})
+	it('Should click on Travel link category', () => {
+		cy.get('a').contains('Travel').click()
+		cy.get('h1').contains('Travel')
+	})
+	it('Should display correct number of books', () => {
+		cy.get('.product_pod').its('length').should('eq', 11)
+	})
+})
