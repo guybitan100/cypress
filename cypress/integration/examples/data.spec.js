e7c640c6c60668a764ad7316c260350e16aacc22
Guy Bitan <guy.bitan@gmail.com>
2022-01-11 09:43:40 +0200

    Add

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -1,8 +1,14 @@
 describe('Write / Read Data to Json / Text File', () => {
-	it('should write data to json', () => {
+	it('should write data to json file', () => {
 		cy.writeFile('log.json', { name: 'Guy', age: 29 })
 	})
 	it('should write data to text file', () => {
 		cy.writeFile('log.txt', 'Hello World')
+	})
+	it('should read data to json file', () => {
+		cy.readFile('log.json').its('age').should('eq', 25)
+	})
+	it('should read data to txt file', () => {
+		cy.readFile('log.txt').its('age').should('eq', 'Hello World')
 	})
 })
