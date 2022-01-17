87cfcca06b2056b7fb7f01c3c0b99b27bed0f74e
Guy Bitan <guy.bitan@gmail.com>
2022-01-17 05:56:38 +0200

    Add

--- cypress/integration/examples/before-class-after-skip.spec.js
+++ cypress/integration/examples/before-class-after-skip.spec.js
@@ -22,11 +22,23 @@ class HomePage extends BasePage {
 
 //Tests
 describe('Scrolling the page', () => {
-	it('should scroll up and down the page', () => {
+	before('Load Home Page', () => {
 		HomePage.loadHomePage()
+	})
+	beforeEach('Run before each it block in the describe', () => {})
+	it('should scroll up and down the page', () => {
 		HomePage.scrollToBottom()
 		HomePage.wait(5000)
 		HomePage.scrollToTop()
 		HomePage.wait(5000)
 	})
+	it.only('Only This step - should scroll up and down the page', () => {
+		HomePage.scrollToBottom()
+		HomePage.wait(5000)
+		HomePage.scrollToTop()
+		HomePage.wait(5000)
+	})
+	it.skip('skip this step', () => {})
+	afterEach('Run after each it block in the describe', () => {})
+	after('Run After All Tests inside describe block are done', () => {})
 })
