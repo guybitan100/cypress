3399af6d5a28524f4c2bbb2fd97e1ca118515e3f
Guy Bitan <guy.bitan@gmail.com>
2022-01-17 05:32:08 +0200

    Add

--- cypress/integration/examples/class.spec.js
+++ cypress/integration/examples/class.spec.js
@@ -1,8 +1,28 @@
-class BasePage
+class BasePage {
+	static loadHomePage() {
+		cy.visit('https://devexpress.github.io/testcafe/example/')
+	}
+
+	static wait(number) {
+		cy.wait(number)
+	}
 }
 
+class HomePage extends BasePage {
+	static scrollToBottom() {
+		cy.get('#submit-button').scrollIntoView()
+	}
+
+	static scrollToTop() {
+		cy.get('header').scrollIntoView()
+	}
+}
 describe('Scrolling the page', () => {
 	it('should scroll up and down the page', () => {
-		cy.visit('https://devexpress.github.io/testcafe/example/')
+		HomePage.loadHomePage()
+		HomePage.scrollToBottom()
+		HomePage.wait(5000)
+		HomePage.scrollToTop()
+		HomePage.wait(5000)
 	})
 })
